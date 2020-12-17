9bdaa2c912a4c6e163705e40ef69a078f40bbb8a
cgcarlosg <cgcarlosg@hotmail.com>
2020-12-17 09:53:33 -0500

    add the delete method

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -5,6 +5,32 @@ class ArticlesController < ApplicationController
 
       def show
         @article = Article.find(params[:id])
+      end
+
+      def new
+        @article = Article.new
+      end
+
+      def create
+        @article = Article.new(@article_params)
+        @article.save
+        redirect_to article_path(@article)
+      end
+
+      def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash.notice = "Article '{@article.title}' Destroyed"
+        redirect_to article_path
+      end
+
+      def edit
+        @article = Article.find(params[:id])
+        @article.update(article_params)
+
+        redirect_to article_path
 
       end
+      
+
 end
